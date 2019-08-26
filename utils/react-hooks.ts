00d3d53b12e219a3c700fe03693375877423a85a
KuwabataK <monorod98@gmail.com>
2019-08-26 20:54:38 +0900

    add usePrevious

---
+++ utils/react-hooks.ts
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from "react";
+
+/**
+ * 引数に指定した値を保存し、次のレンダリング時に返り値として所得することができます。
+ * 初回レンダリング時の返り値は undefined になります
+ * 
+ * @param value 保存する値
+ */
+export function usePrevious<T>(value: T): T | undefined {
+    // The ref object is a generic container whose current property is mutable ...
+    // ... and can hold any value, similar to an instance property on a class
+    const ref = useRef();
+
+    // Store current value in ref
+    useEffect(() => {
+        (ref.current as any) = value;
+    }, [value]); // Only re-run if value changes
+
+    // Return previous value (happens before update in useEffect above)
+    return (ref.current as unknown as T);
+}
