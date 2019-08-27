9a00084ab6f16ebec049661e3b9cfeee2752e131
KuwabataK <monorod98@gmail.com>
2019-08-28 00:15:30 +0900

    mobxのconfigを変更

--- store/store.ts
+++ store/store.ts
@@ -1,4 +1,7 @@
 import { CounterStore } from "./CounterStore";
+import { configure } from "mobx";
+
+configure({ enforceActions: 'always' })
 
 export default {
     counterStore: new CounterStore()
