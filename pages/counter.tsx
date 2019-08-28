83ee27b5f5c5cafa3164205fa78ed96d7234978f
KuwabataK <monorod98@gmail.com>
2019-08-29 02:02:12 +0900

    add lodash and detect change test

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -3,23 +3,41 @@ import Link from 'next/link'
 import Layout from '../src/components/template/Layout'
 import store from '../src/store/store'
 import { observer } from 'mobx-react'
+import { usePrevious } from '../src/utils/react-hooks'
+import { cloneDeep } from 'lodash'
 
 const CounterPage: React.FunctionComponent = observer(() => {
+  const preCnt = usePrevious(cloneDeep(store.counterStore.objectCounter))
+  // const preCnt = usePrevious(store.counterStore.objectCounter)
+  console.log(preCnt && preCnt.counter)
+  console.log(preCnt && preCnt.deepcnt.counter)
+
   const increment: () => void = () => {
-    store.counterStore.increment()
+    store.counterStore.incrementObj()
   }
 
   const decrement: () => void = () => {
-    store.counterStore.decrement()
+    store.counterStore.decrementObj()
+  }
+
+  const incrementDeep: () => void = () => {
+    store.counterStore.incrementDeep()
+  }
+
+  const decrementDeep: () => void = () => {
+    store.counterStore.decrementDeep()
   }
 
   return (
     <Layout title="カウンターページ | Next.js + TypeScript Example">
       <h1>カウンターページ</h1>
       <p>mobxを使ったカウンターページのサンプルです</p>
-      <p>{store.counterStore.counter}</p>
+      <p>{store.counterStore.objectCounter.counter}</p>
+      <p>deep: {store.counterStore.objectCounter.deepcnt.counter}</p>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
+      <button onClick={incrementDeep}>deep+</button>
+      <button onClick={decrementDeep}>deep-</button>
       <p>
         <Link href="/">
           <a>Go home</a>
--- src/store/CounterStore.ts
+++ src/store/CounterStore.ts
@@ -2,6 +2,12 @@ import { observable, action } from 'mobx'
 
 export class CounterStore {
   @observable counter = 0
+  @observable.ref objectCounter = {
+    counter: 0,
+    deepcnt: {
+      counter: 0
+    }
+  }
 
   @action
   increment(): void {
@@ -11,5 +17,29 @@ export class CounterStore {
   @action
   decrement(): void {
     this.counter--
+  }
+
+  @action
+  incrementObj(): void {
+    this.objectCounter.counter++
+    this.objectCounter = { ...this.objectCounter }
+  }
+
+  @action
+  decrementObj(): void {
+    this.objectCounter.counter--
+    this.objectCounter = { ...this.objectCounter }
+  }
+
+  @action
+  incrementDeep(): void {
+    this.objectCounter.deepcnt.counter++
+    this.objectCounter = { ...this.objectCounter }
+  }
+
+  @action
+  decrementDeep(): void {
+    this.objectCounter.deepcnt.counter--
+    this.objectCounter = { ...this.objectCounter }
   }
 }
