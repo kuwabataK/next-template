1e0b68c73595887ee86b7d0240434ff26b39a835
KuwabataK <monorod98@gmail.com>
2019-09-05 15:14:05 +0900

    add comment

--- src/workers/example.worker.ts
+++ src/workers/example.worker.ts
@@ -3,6 +3,7 @@
 const ctx: Worker = self as any
 
 ctx.addEventListener('message', async event => {
+  console.log('worker側だよ！！')
   console.log('example.worker received 1:', event)
   ctx.postMessage({ msgCode: 'S', msg: '' })
 })
